7a5e64b536d36309bd40e8d7935c6a6b3866c328
Nicholai Dimov <ndimov@users.noreply.github.com>
2022-07-19 23:13:40 -0700

    Add markers, rounded roads, zorder colors

--- draw.py
+++ draw.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from numpy import full
 import osmnx as ox
 from osmnx import utils_geo
 import re
@@ -6,6 +7,30 @@ import colorsys
 import matplotlib.colors as mc
 import matplotlib.pyplot as plt
 from data import presidents, states
+from PIL import Image
+
+from matplotlib.backend_bases import GraphicsContextBase, RendererBase
+import matplotlib.pyplot as plt
+
+import types
+
+# hacks to make the line caps rounded
+# https://stackoverflow.com/questions/11578760/matplotlib-control-capstyle-of-line-collection-large-number-of-lines
+class GC(GraphicsContextBase):
+    def __init__(self):
+        super().__init__()
+        self._capstyle = "round"
+        # self._joinstyle = "round"
+
+
+def custom_new_gc(self):
+    return GC()
+
+
+RendererBase.new_gc = types.MethodType(custom_new_gc, RendererBase)
+
+# ----------------------------------------------------------------------
+
 
 kws = defaultdict(set)
 
@@ -21,8 +46,8 @@ cities = sorted(cities, key=lambda x: x[0])  # sort by city name
 
 GRADIENTS = {
     "num": ("#e00000", [str(i) for i in range(1, 100)]),
-    "presidents": ("#00ff00", presidents),
-    "states": ("#26e026", states),
+    "presidents": ("#26e026", presidents),
+    "states": ("#4370cd", states),
 }
 PLACEHOLDER = "#444444"
 
@@ -36,14 +61,18 @@ def gradient_color(name: str, street_name: str):
             condition = match_value == street_name
         elif name == "presidents":
             condition = (
-                any(match_value == keyword for keyword in street_name.split(" "))
+                any(match_value == keyword for keyword in street_name.split())
                 and not "pierce elevated" in street_name  # houston
             )
         else:  # "states"
-            condition = match_value in street_name
+            condition = " " + match_value in " " + street_name
+            # and all(
+            #     match_piece in street_name.split()
+            #     for match_piece in match_value.split()
+            # )
         if condition:
             kws[match_value].add(street_name)
-            # print("MATCH:", match_value, "STREET NAME:", street_name)
+            # print("MATCH:", match_value, "STREET NAME:", street_name, "BASE:", base)
             return base
             # clr = colorsys.rgb_to_hsv(*mc.to_rgb(base))
             # return colorsys.hsv_to_rgb(
@@ -87,18 +116,48 @@ def plot_center(coords, filename):
         simplify=False,  # see SW 1st becoming SW Washington in Portland
     )
     bbox = utils_geo.bbox_from_point((lat, long), radius)
-    ec = [road_color(data.get("name")) for u, v, data in roads.edges(data=True)]
-    fig, ax = ox.plot_graph(
-        roads,
-        bbox=bbox,
-        node_size=0,
-        edge_color=ec,
-        edge_linewidth=1.8,
-        show=False,
-        close=False,
-        bgcolor="#fafafa",
-        # bgcolor="#00000000",  # transparent
-    )
+    edges_with_colors = [
+        ((u, v, k), road_color(data.get("name")))
+        for u, v, k, data in roads.edges(keys=True, data=True)
+    ]
+    subgraphs = [
+        (
+            roads.edge_subgraph(
+                [edge for edge, clr in edges_with_colors if clr == desired_clr]
+            ),
+            desired_clr,
+        )
+        # most to least common
+        for desired_clr in [PLACEHOLDER]
+        + [GRADIENTS[name][0] for name in ["num", "presidents", "states"]]
+    ]
+    # edge_zorder = []
+    # for val in ec:
+    #     zorder = 10
+    #     if val == GRADIENTS["num"][0]:
+    #         zorder = 20
+    #     elif val == GRADIENTS["presidents"][0]:
+    #         zorder = 30
+    #     elif val == GRADIENTS["states"][0]:
+    #         zorder = 40
+    #     edge_zorder.append(zorder)
+    ax = None
+    for subgraph, desired_clr in subgraphs:
+        if len(subgraph.edges()) == 0:
+            continue
+        fig, ax = ox.plot_graph(
+            subgraph,
+            ax=ax,
+            bbox=bbox,
+            node_size=0,
+            edge_color=desired_clr,
+            edge_linewidth=1.8,
+            # edge_zorder=edge_zorder,
+            show=False,
+            close=False,
+            bgcolor="#fafafa",
+            # bgcolor="#00000000",  # transparent
+        )
     # fig, ax = ox.plot_figure_ground(
     #     roads,
     #     node_size=0,
@@ -108,14 +167,32 @@ def plot_center(coords, filename):
     #     close=False,
     #     bgcolor="#000000ff",
     # )
-    ax.scatter(long, lat, c="#dd0000")
+    # ax.scatter(long, lat, c="#dd0000")
     # remove the padding
     # ax.get_xaxis().set_visible(True)
     # ax.get_yaxis().set_visible(True)
     # plt.Circle((long, lat), radius, color="#aaaaaa", fill=False)
+    # line_collection = ax.get_children()[0]
+    # for line in line_collection.lines:
+    #     print(line)
 
     fig.set_frameon(False)
-    fig.savefig(f"{filename}.png", dpi=300, bbox_inches="tight", pad_inches=0)
+    full_filename = f"{filename}.png"
+    fig.savefig(full_filename, dpi=300, bbox_inches="tight", pad_inches=0)
+
+    img = Image.open(full_filename)
+    img_w, img_h = img.size
+    marker = Image.open("pin-jacqui-b.png").convert("RGBA")
+    old_marker_w, old_marker_h = marker.size
+    marker = marker.resize((100, int(old_marker_h / old_marker_w * 100)))
+    marker_w, marker_h = marker.size
+    img.paste(
+        # btm mid of marker is exact center of img
+        marker,
+        ((img_w - marker_w) // 2, img_h // 2 - marker_h),
+        marker,  # for transparency
+    )
+    img.save(full_filename)
     plt.close()
 
 
@@ -127,10 +204,13 @@ def to_lat_long(center):
 for i, (city, stn_coord, city_hall_coord) in enumerate(cities):
     print(city)
     # TODO: strip out relevant coords or whatever
-    coords = sorted(
-        [to_lat_long(coord) for coord in [stn_coord, city_hall_coord]],
-        key=lambda x: x[1],
-    )
+    # doesn't seem too necessary. honestly props to them if they can do it,
+    # seems more fun than the actual id part
+    coords = list(map(to_lat_long, [city_hall_coord, stn_coord]))
+    # coords = sorted(
+    #     [to_lat_long(coord) for coord in [stn_coord, city_hall_coord]],
+    #     key=lambda x: x[1],
+    # )
     plot_center(coords[0], f"{i+1}a")
     plot_center(coords[1], f"{i+1}b")
 
